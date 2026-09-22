5e7771e939d367aecad478b9004fe7fa673599fd
Christoffer <bendigsunne@hotmail.com>
2026-09-22 10:12:17 +0200

    made a function

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+let list = document.getElementById("list");
+
+list.appendChild(addListItem("healthy", list.children.length + 1, "Milk"));
+
+function addListItem(className, id, text) {
+    let item = document.createElement("li");
+    item.setAttribute("id", id);
+    item.setAttribute("class", className);
+    let textNode = document.createTextNode(text);
+    item.appendChild(textNode);
+    return item;
+}
